ba4a80fd7e3ddc3f304f14952ade69a3c2d245aa
carlos-espinoza.io <esp16013@byui.edu>
2020-08-15 03:50:50 -0700

    update5

--- index.php
+++ index.php
@@ -387,14 +387,15 @@ function carousel() {
       </p>
     </form>
 -->
-    <form method="post" action="sendEmail.php">
-	<label for="name">Name:</label>
-	<input type="text" class="text" name="cf_name" id="name" />
-	<label for="email">email:</label>
-	<input type="text" class="text" name="cf_email" id="email" />
-	<label for="text">Message:</label>
-	<textarea class="text" name="cf_message" id="message"></textarea>
-	<input type="submit" class="submit" value="Send" />
+    <form action="mailto:carjun2@hotmail.com" method="post" enctype="text/plain">
+Name:<br>
+<input type="text" name="name"><br>
+E-mail:<br>
+<input type="text" name="mail"><br>
+Comment:<br>
+<input type="text" name="comment" size="50"><br><br>
+<input type="submit" value="Send">
+<input type="reset" value="Reset">
 </form>
       
     <!-- Image of location/map -->
