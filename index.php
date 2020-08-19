788d768176670cd07074ea8bb17d2920b67cd8f9
carlos-espinoza.io <esp16013@byui.edu>
2020-08-18 20:35:11 -0700

    test

--- index.php
+++ index.php
@@ -34,6 +34,7 @@ body, html {
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta content="text/html; charset=iso-8859-2" http-equiv="Content-Type">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<?php include 'form_process.php'; ?> 
 <style>
 .mySlides {display:none;}
 </style>
@@ -387,68 +388,36 @@ function carousel() {
       </p>
     </form>
 -->
-     <center>
-		<h4 class="sent-notification"></h4>
-
-		<form id="myForm">
-			<h2>Send an Email</h2>
-
-			<label>Name</label>
-			<input id="name" type="text" placeholder="Enter Name">
-			<br><br>
-
-			<label>Email</label>
-			<input id="email" type="text" placeholder="Enter Email">
-			<br><br>
-
-			<label>Subject</label>
-			<input id="subject" type="text" placeholder=" Enter Subject"> 
-			<br><br>
-
-			<p>Message</p>
-			<textarea id="body" rows="5" placeholder="Type Message">
-            </textarea>    
-			<br><br>
-
-                <button type="button" onclick="sendEmail()" value="Send An Email">Submit</button> 
-		</form>
-	</center>
-            <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
-	<script type="text/javascript">
-        function sendEmail() {
-            var name = $("#name");
-            var email = $("#email");
-            var subject = $("#subject");
-            var body = $("#body");
-
-            if (isNotEmpty(name) && isNotEmpty(email) && isNotEmpty(subject) && isNotEmpty(body)) {
-                $.ajax({
-                   url: 'sendEmail.php',
-                   method: 'POST',
-                   dataType: 'json',
-                   data: {
-                       name: name.val(),
-                       email: email.val(),
-                       subject: subject.val(),
-                       body: body.val()
-                   }, success: function (response) {
-                        $('#myForm')[0].reset();
-                        $('.sent-notification').text("Message Sent Successfully.");
-                   }
-                });
-            }
-        }
-
-        function isNotEmpty(caller) {
-            if (caller.val() == "") {
-                caller.css('border', '1px solid red');
-                return false;
-            } else
-                caller.css('border', '');
-
-            return true;
-        }
-    </script>
+      <div class= "cotainer">
+          
+    <form id="contact" action=" <?= $_SERVER['PHP_SELF']; ?> " method = "post">
+        <h3> Quick Contact</h3>
+        <h4>Contact us today, and get reply with in 24 hours!</h4>
+        <fieldset>
+            <input placeholder="Your name" type="text" tabindex="1" name="name" value="<? = $name ?> " autofocus>
+            <span class="error"><? $name_error ?></span>
+        </fieldset>
+         <fieldset>
+            <input placeholder="Your Email Address" type="text" name="email" value="<? = $email ?> " tabindex="2" >
+            <span class="error"><? $email_error ?></span>
+        </fieldset>
+         <fieldset>
+            <input placeholder="Your Phone Number" type="text" name="phone" value="<? = $phone ?> " tabindext="3" >
+            <span class="error"><? $phone_error ?></span>
+        </fieldset>
+         <fieldset>
+            <input placeholder="Your Web Site starts with Http://" type="text" name="url" value="<? = $url ?> " tabindex="4">
+            <span class="error"><? $url_error ?></span>
+        </fieldset>
+         <fieldset>
+            <input placeholder="Type your Message Here..." type="text" name="message" value="<? = $message ?> " tabindex ="5">
+            
+        </fieldset>
+        <fieldset>
+        <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+        </fieldset>
+        </form>
+          </div>
       
     <!-- Image of location/map -->
     <div class="w3-xlarge w3-section">
