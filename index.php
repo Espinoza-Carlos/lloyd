0797af29aa608f23230575481a51e2642061682d
carlos-espinoza.io <esp16013@byui.edu>
2020-08-18 19:50:59 -0700

    test

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ body, html {
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta content="text/html; charset=iso-8859-2" http-equiv="Content-Type">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<?php include('form_process.php'); ?>
+<?php include 'form_process.php'; ?>
 <style>
 .mySlides {display:none;}
 </style>
