aa8dcf90f7e696e532578424d5f47f5bd83a1122
carlos-espinoza.io <esp16013@byui.edu>
2020-08-19 22:03:21 -0700

    hello

--- Construction Clean.php
+++ Construction Clean.php
@@ -1,19 +1,130 @@
 <!DOCTYPE html>
 <html>
-<head>
+<title>Lloyd  Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="webform.css" media="all">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>    
+    <script src="main.js"></script>    
 <style>
-body {
-    background-image: url('construction clean.jpg');
-    opacity: 1;
-    text-align: center;
+body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
+
+body, html {
+  height: 100%;
+  line-height: 1.8;
+}
+
+/* Full height image header */
+.bgimg-1 {
+  background-position: center;
+  background-size: cover;
+  background-image: url("");
+  min-height: 100%;
+}
+
+.w3-bar .w3-button {
+  padding: 16px;
 }
 </style>
-</head>
+       <head>
+<title>LLoyd</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta content="text/html; charset=iso-8859-2" http-equiv="Content-Type">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+.mySlides {display:none;}
+</style>
+</head> 
 <body>
 
-<h2>Background Image</h2>
+<!-- Navbar (sit on top) -->
+    
+<div class="w3-top">
+  <div class="w3-bar w3-white w3-card" id="myNavbar">
+    <a href="index.php" class="w3-bar-item w3-button w3-wide">HOME</a>
+    <!-- Right-sided navbar links 
+    <div class="w3-right w3-hide-small">
+      <a href="#about" class="w3-bar-item w3-button">ABOUT</a>
+      <a href="#team" class="w3-bar-item w3-button"><i class="fa fa-user"></i> TEAM</a>
+      <a href="#work" class="w3-bar-item w3-button"><i class="fa fa-th"></i> WORK</a>
+      <a href="#pricing" class="w3-bar-item w3-button"><i class="fa fa-usd"></i> PRICING</a>
+      <a href="#contact" class="w3-bar-item w3-button"><i class="fa fa-envelope"></i> CONTACT</a>
+    </div>-->
+    <!-- Hide right-floated links on small screens and replace them with a menu icon --><!-- <img src="ML%20Cleaning%20logo.jpg" style="width:7%">-->
+                                                                                        
+    <a href="javascript:void(0)" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onclick="w3_open()">
+      <i class="fa fa-bars"></i>
+    </a>
+  </div>
+</div>
 
-<p>some text will be here</p>
+
+<!-- Page content -->
+<div class="w3-content" style="max-width: 1100px; width: 100%; height: 100px;">
+
+  <!-- About Section -->
+  <img class="w3-image" src="construction clean.jpg" alt="Hamburger Catering" width="max" height="max">
+  
+	<br>
+	<br><br><br><br><br>
+	
+<!--  </div>width="1242" height="64734">-->
+    
+	
+
+    <div class="fa-address-card">
+<h1 class="w3-center"><strong>Construction Cleaning</strong></h1>
+<br>
+      <h5 class="w3-center">Every Detail Counts</h5>
+      <p class="w3-large">Ridgecrest is growing, and is home to many new development, constructin, and remodelling porjects. </p>
+      <p class="w3-large">This Projects can be messy. </p>
+      <p class="w3-large">On ML Our our goal in handling your cleanup project is to work efficiently and effectively.</p>
+      <h3 id="1789643328" data-element-type="paragraph" data-uialign="left"><strong><em>Construction Cleaning</em></strong></h3>
+      <div id="1799134103" data-uialign="left">
+        <ul>
+          <li>Vacuuming and dusting of all surfaces and upholstery, from ceilings and walls to floors</li>
+          <li>Cleaning the tops of doors, door knobs, hardware, moldings</li>
+          <li>Complete disinfection of kitchens and bathrooms</li>
+          <li>Gentle cleaning of floors, baseboards, cabinets, counters, light fixtures, appliances</li>
+          <li>All glass, stone, and metal surfaces gently cleaned</li>
+          <li>Dust removal from blinds, ceiling pipes/fans, air ducts, vents, light fixtures</li>
+          <li>Window cleaning, including sills, window frames, and glass</li>
+          <li>Desk and cabinet cleaning (for offices)</li>
+          <li>HEPA filter vacuums and air machines provided for maximum dust removal</li>
+        </ul>
+        <p>&nbsp;</p>
+      </div>
+      <blockquote>
+        <blockquote>
+          <blockquote>
+            <blockquote>
+              <blockquote>
+                <blockquote>
+                  <blockquote>
+                    <blockquote>
+                      <blockquote>&nbsp; </blockquote>
+                    </blockquote>
+                  </blockquote>
+                </blockquote>
+              </blockquote>
+            </blockquote>
+          </blockquote>
+        </blockquote>
+      </blockquote>
+      <p class="w3-large w3-text-grey w3-hide-medium">&nbsp;</p>
+  </div>
+</div>
+  
+  <hr>
+  
+ 
+<!-- End page content -->
+
+
 
 </body>
+	<!-- Footer -->
 </html>
